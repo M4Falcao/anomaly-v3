dd4806ec44dc8437585cfae75e80a160749a1a08
Mateus Falcão <73663282+M4Falcao@users.noreply.github.com>
2025-12-08 19:24:39 -0300

    ajuste score inicial

--- anomaly-detection-pesquisa/differnet/train.py
+++ anomaly-detection-pesquisa/differnet/train.py
@@ -72,7 +72,7 @@ class Score_Observer:
 
     def update(self, score, epoch, print_score=False):
         self.last = score
-        if epoch == 0 or score > self.max_score:
+        if self.max_score is None or score > self.max_score: # Fix for NoneType comparison")
             self.max_score = score
             self.max_epoch = epoch
         if print_score:
